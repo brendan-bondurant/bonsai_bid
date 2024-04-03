ea61ca211193ae58e742cdac0e38150c846a84c8
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-04-03 15:56:33 -0600

    Adjust feedback model spec

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -12,6 +12,5 @@ RSpec.describe Feedback, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:rating) }
     it { should validate_inclusion_of(:rating).in_range(1..5) }
-    it { should validate_presence_of(:comment) }
   end
 end
