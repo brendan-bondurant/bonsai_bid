ca364761f00b127da3f679bf1e5b34447d9ef40d
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-04-03 16:01:10 -0600

    fix search test that was broken in last commit

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -39,15 +39,14 @@ RSpec.describe 'Item search', type: :feature do
     visit root_path
     fill_in 'search', with: 'bush'
     click_button 'Search'
-    
     expect(page).to have_content(@item1.name)
     click_link("#{@item1.name}")
-    category = Category.find(@item1.category_id)
+    
     expect(page).to have_content("#{@item1.name}")
     expect(page).to have_content("#{@item1.description}")
     expect(page).to have_content("#{@item1.starting_price}")
     expect(page).to have_content("#{@item1.current_price}")
-    expect(page).to have_content("#{category.name}")
+    expect(page).to have_content("#{@item1.category_id}")
     expect(page).to have_content("#{@item1.buy_it_now_price}")
     expect(page).to have_content("#{@item1.start_date}")
     expect(page).to have_content("#{@item1.end_date}")
