9cf36f536894816ec8cb3a6debb65273936d02a4
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-04-04 14:04:33 -0600

    Model testing at 100

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,6 @@ class Item < ApplicationRecord
 
   belongs_to :seller, class_name: 'User'
   belongs_to :category
-  # has_many :bids
   has_many :feedbacks
   has_many :watchlists
   has_one :sale_transaction
@@ -12,21 +11,7 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  # validates :starting_price, numericality: { greater_than_or_equal_to: 0 }
-  # validates :current_price, numericality: { greater_than_or_equal_to: 0 }
-  # validates :buy_it_now_price, numericality: { greater_than_or_equal_to: 0 }
-  # validates :start_date, presence: true
-  # validates :bid_increment, presence: true
-  # validates :end_date, presence: true
   validates :status, presence: true, inclusion: { in: %w(listed active sold ended) }
   validates :status, presence: true
-
-
-  # after_initialize :set_default_status, if: :new_record?
   
-  private
-
-  def set_default_status
-    self.status ||= :listed
-  end
 end
