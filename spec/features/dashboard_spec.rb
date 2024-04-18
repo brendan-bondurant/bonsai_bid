1b898467b11a35c976636ffc84860534c645a254
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-04-18 14:05:57 -0600

    Dashboard test is adjusted

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
-RSpec.feature 'Dashboard - User Items and Watchlist', type: :feature do
-  let(:user) { FactoryBot.create(:user) }
-  let(:other_user) { FactoryBot.create(:user) }
-  let(:listed_item) { FactoryBot.create(:item, seller: user) }
-  let(:other_item) { FactoryBot.create(:item, seller: other_user) }
+RSpec.feature 'Dashboard - User Auctions and Watchlist', type: :feature do
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let(:listed_item) { create(:item, seller: user) }
+  let(:other_item) { create(:item, seller: other_user) }
+  let!(:user_auction) { create(:auction, item: listed_item) }
+  let!(:other_auction) { create(:auction, item: other_item) }
 
   before do
     login_as(user, scope: :user)
-
-    visit item_path(other_item)
-    click_link 'Watch this Item'
+    # Simulating adding an auction to the watchlist; this could also be done directly if the link triggers an immediate action
+    visit auction_path(other_auction)
+    click_link 'Watch this Auction'
   end
 
-  scenario 'User views listed and watched items on dashboard' do
+  scenario 'User views their auctions and watched auctions on dashboard' do
     visit dashboard_user_path(user)
 
-    expect(page).to have_content(listed_item.name)
-    
-    expect(page).to have_content(other_item.name)
+    # Checking for user's own auction
+    expect(page).to have_content(user_auction.item.name)
+
+    # Checking for an auction from another user that is being watched
+    expect(page).to have_content(other_auction.item.name)
   end
 end
